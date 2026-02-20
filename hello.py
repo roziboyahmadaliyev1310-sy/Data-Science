29a89d7057825bdba2624c9e8cd98dea740add7a
A_Roziboy <roziboyahmadaliyev1310@gmail.com>
2026-02-21 05:32:09 +0900

    object orenter programming

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
 print('salom')
+
+class Workers:
+    def __init__(self,worker):
+        self.worker=worker
+
+    def age_worker(self):
+        return self.age
+
+    def name_worker(self):
+        return self.worker
+
+
+work="Ali"
+
+
+tracker=Workers(work)
+print(tracker.name_worker())
